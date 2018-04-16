59a19f9c13ad8484eb8a7490eeb4143aefeb7dd4
Frank McCown <fmccown@harding.edu>
2018-04-16 14:36:39 -0500

    Add project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # UltimateTicTacToeMinimax
+
+Starter project for implementing minimax for Ultimate Tic-Tac-Toe.
---
+++ UltimateTicTacToeMinimax/Bot/SmartBot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UltimateTicTacToeMinimax.Bot
+{
+    public class SmartBot
+    {
+        private const bool Debug = true;
+
+        private Random rand = new Random();
+
+        /// <summary>
+        /// Returns the next move to make. Edit this method to make your bot smarter.
+        /// Currently does only random moves.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns>The column where the turn was made</returns>
+        public Move GetMove(BotState state)
+        {
+            var moves = state.UltimateBoard.AvailableMoves;
+
+            if (moves.Count > 0)
+            {
+                // get random move from available moves
+                return moves[rand.Next(moves.Count)]; 
+                //return moves.First();               
+            }
+
+            // pass
+            return null;       
+        }
+                
+        static void Main(string[] args)
+        {
+            BotParser parser = new BotParser(new SmartBot());
+            parser.Run();
+        }
+    }
+}
